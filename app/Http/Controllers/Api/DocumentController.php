8fd810ed486e1d7583d4808dfdacabf8cd328385
darkropo <rodolfo.noguera@gmail.com>
2022-05-09 13:37:53 -0500

    Changes to class DocumentController add trait and models

--- app/Http/Controllers/Api/DocumentController.php
+++ app/Http/Controllers/Api/DocumentController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Traits\DocumentUseTrait;
 
 class DocumentController extends Controller
 {
+    use DocumentUseTrait;
     /**
      * Display a listing of the resource.
      *
@@ -100,35 +102,35 @@ class DocumentController extends Controller
             $data['planSubTypes'] = isset($inputData['planSubTypes']) ? $inputData['planSubTypes'] : null;
             if (!empty($inputData['sortedBy'])) {
                 $sortBy = $inputData['sortedBy'];
-                if ($sortBy == 'name') {
+                switch ($sortBy) {
+                    case 'name':
+                        $data['sortedBy'] = 'name';
+                        break;
+                    case 'docType':
+                        $data['sortedBy'] = 'type';
+                        break;
+                    case 'planTypes':
+                        $data['sortedBy'] = 'plan_type';
+                        break;
+                    case 'file_extension':
+                        $data['sortedBy'] = 'file_extension';
+                        break;
+                    case 'created_at':
+                        $data['sortedBy'] = 'created_at';
+                        break;
+                    
+                    default:
                     $data['sortedBy'] = 'name';
-                } elseif ($sortBy == 'docType') {
-                    $data['sortedBy'] = 'type';
-                } elseif ($sortBy == 'planTypes') {
-                    $data['sortedBy'] = 'plan_type';
-                } elseif ($sortBy == 'file_extension') {
-                    $data['sortedBy'] = 'file_extension';
-                } elseif ($sortBy == 'created_at') {
-                    $data['sortedBy'] = 'created_at';
-                } else {
-                    $data['sortedBy'] = 'name';
+                        break;
                 }
             }
             if ($UserData['is_user'] == 'General Contractor') {
-                $mergedProject = ProjectInvitationMailSent::where('project_id', $data['projectId'])
-                    ->where('emailId', $UserData['email'])
-                    ->Where(function ($subquery) {
-                        $subquery->orwhere('is_merged', true)
-                            ->orWhere('is_approved_merge', true);
-                    })->first();
+                $mergedProject = $this->inviteMailSent($data['projectId'],$UserData['email']);
                 if ($mergedProject) {
                     $data['projectId'] = $mergedProject['merged_project_id'];
                 }
                 if (empty($data['showFilesForGcComp'])) {
-                    $gcForFile = ProjectInvitationMailSent::where('project_id', $data['projectId'])
-                        ->where('gc_project_invitation_mail_sent.is_original_gc', true)
-                        ->leftjoin('user_profile', 'user_profile.id_user', 'gc_project_invitation_mail_sent.user_id')
-                        ->first();
+                    $gcForFile = $this->getGCForfile($data['projectId']);
                     if (!empty($gcForFile)) {
                         $data['showFilesForGcComp'] = $gcForFile['id_company'];
                     } else {
@@ -140,8 +142,7 @@ class DocumentController extends Controller
             if (!empty($docType)) {
                 $data['docType'] = $docType;
             }
-            $getAllProjectObj = new ProjectFiles();
-            $result = $getAllProjectObj->getsortedFiles($data, $UserData);
+            $result = $this->getsortedFiles($data, $UserData);
             if (!empty($result)) {
                 $returnData = $result;
             } else {
@@ -151,95 +152,5 @@ class DocumentController extends Controller
             $error = ApiConstant::DATA_NOT_FOUND;
         }
         return $this->returnableResponseData($returnData, $error);
-    }
-
-    public function getsortedFiles($data, $UserData)
-    {
-        $count = $count_plans = $count_specs = $count_general = $count_addendums = $count_RFI_response = 0;
-        $docType = $data['docType'] ?? null;
-        $planSubTypes = $data['planSubTypes'] ?? null;
-        if ($UserData['is_user'] == 'General Contractor') {
-            $result = ProjectFiles::where("id_project", $data['projectId'])
-                ->where(function ($sub_query) use ($UserData, $docType, $planSubTypes) {
-                    $sub_query->orwhere('gc_company_id', '9999999')
-                        ->orWhere('gc_company_id', $UserData['id_company']);
-                    if ($docType) {
-                        $sub_query->where('type', $docType);
-                    }
-                    if ($planSubTypes) {
-                        $sub_query->where('plan_type', $planSubTypes);
-                    }
-                })
-                ->groupBy(['name', 'type'])
-                ->orderBy($data['sortedBy'], $data['orderBy']);
-        } else {
-            $result = ProjectFiles::where(function ($sub_query) use ($data, $docType, $planSubTypes) {
-                if ($data['showFilesForGcComp']) {
-                    $sub_query->where('gc_company_id', $data['showFilesForGcComp'])
-                        ->where('id_project', $data['projectId']);
-                } else {
-                    $all_projects = ProjectInvitationMailSent::where('project_id', $data['projectId'])->get()
-                        ->pluck('merged_project_id')
-                        ->unique()
-                        ->values()
-                        ->toArray();
-                    $all_projects[] = $data['projectId'];
-                    $sub_query->whereIn('id_project', array_filter($all_projects));
-                }
-                if ($docType) {
-                    $sub_query->where('type', $docType);
-                }
-                if ($planSubTypes) {
-                    $sub_query->where('plan_type', $planSubTypes);
-                }
-            })
-                ->groupBy(['name', 'type'])
-                ->orderBy($data['sortedBy'], $data['orderBy']);
-        }
-        $all_data_count = $result->get();
-        $all_data = $result->offset($data['skip'])
-            ->limit($data['limit'])
-            ->get();
-
-
-        if (!empty($all_data_count)) {
-            foreach ($all_data_count as $single) {
-                $count++;
-                if ($single['type'] == "plans") {
-                    $count_plans++;
-                } elseif ($single['type'] == "specs") {
-                    $count_specs++;
-                } elseif ($single['type'] == "general") {
-                    $count_general++;
-                } elseif ($single['type'] == "addendums") {
-                    $count_addendums++;
-                } elseif ($single['type'] == "RFI_response") {
-                    $count_RFI_response++;
-                }
-            }
-        }
-        if (!empty($all_data)) {
-            $all_files = array();
-            foreach ($all_data as $single) {
-                $allData['docType'] = $single['type'];
-                $allData['planTypes'] = empty($single['plan_type']) ? 'All Other Drawings' : $single['plan_type'];
-                $allData['name'] = $single['name'];
-                $allData['fullPath'] = $single['file_path'];
-                $allData['projectId'] = $single['id_project'];
-                $allData['docId'] = $single['id'];
-                $allData['file_extension'] = $single['file_extension'];
-                $allData['created_at'] = $single['created_at'];
-                $all_files[] = $allData;
-            }
-        }
-        return array(
-            'message' => ApiConstant::DATA_FOUND, "data" => $all_files,
-            'allcount' => $count,
-            'countplans' => $count_plans,
-            'countspecs' => $count_specs,
-            'countgeneral' => $count_general,
-            'countaddendums' => $count_addendums,
-            'countRFI_response' => $count_RFI_response,
-        );
     }
 }
